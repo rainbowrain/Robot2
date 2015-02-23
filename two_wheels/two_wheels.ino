062ca5b171337e5cefe024578484415ff9c1aa01
rainbowrain <rainbowrain@users.noreply.github.com>
2015-02-23 15:16:05 -0500

    Update two_wheels.ino
    Edited header

--- two_wheels/two_wheels.ino
+++ two_wheels/two_wheels.ino
@@ -1,9 +1,9 @@
 /*
 * Brushed_H_Bridge_simple2 sketch
 * commands from serial port control motor direction
-* + or - set the direction, any other key stops the motors
+* + or - set the direction of rotation of the car (+ for CCW, - for CW), any other key stops the motors
 */
-const int in1Pin = 4; // H-Bridge input pins
+const int in1Pin = 4; // H-Bridge for first motor
 const int in2Pin = 2;
 const int in3Pin = 6; // H-Bridge pins for second motor
 const int in4Pin = 7;
@@ -25,7 +25,7 @@ if ( Serial.available()) {
 char ch = Serial.read();
 if (ch == '+')
 {
-Serial.println("CW");
+Serial.println("CCW");
 // first motor
 digitalWrite(in1Pin,LOW);
 digitalWrite(in2Pin,HIGH);
@@ -35,9 +35,10 @@ digitalWrite(in4Pin,LOW);
 }
 else if (ch == '-')
 {
-Serial.println("CCW");
+Serial.println("CW");
 digitalWrite(in1Pin,HIGH);
 digitalWrite(in2Pin,LOW);
+
 digitalWrite(in3Pin,LOW);
 digitalWrite(in4Pin,HIGH);
 }
